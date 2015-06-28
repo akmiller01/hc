4291d71182347472d78694ce6717927e0f57b1af
Alex Miller <alex.k.miller@gmail.com>
2015-06-28 10:59:16 +0100

    Static fix

--- hc/hc/settings.py
+++ hc/hc/settings.py
@@ -113,10 +113,6 @@ STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'data' 'media')
 
